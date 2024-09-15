59ec795821f37f361e3fde0effd3b1259c4b1e2d
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2024-09-15 20:21:56 +0530

    update

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,7 +1,38 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useEffect } from "react";
+import API from "./../auth/axiosIntercept";
+import { getRefreshToken } from "../auth/localStorage";
+import { useDispatch } from "react-redux";
+import { setAccessToken } from "../redux/slices/authSlice.js";
 
-export const AppLayout = () => {
+export const PublicLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export const PrivateRoute = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const refreshToken = getRefreshToken();
+    if (refreshToken) {
+      // Try to refresh the accessToken
+      API.post("/refresh-token", { token: refreshToken })
+        .then(({ data }) => {
+          dispatch(setAccessToken(data.accessToken));
+        })
+        .catch(() => {
+          // Handle error (e.g., redirect to login if refresh fails)
+          window.location.href = "/login";
+        });
+    }
+  }, [dispatch]);
+
   return (
     <div>
       <Navbar />
